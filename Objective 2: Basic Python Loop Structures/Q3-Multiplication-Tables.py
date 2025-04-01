4f49029622ff4b37013fbc1ff9c9411d84046ce8
Travisthefish <148793709+smallbigie@users.noreply.github.com>
2025-04-01 13:43:15 +0000

    Finished

--- Objective 2: Basic Python Loop Structures/Q3-Multiplication-Tables.py
+++ Objective 2: Basic Python Loop Structures/Q3-Multiplication-Tables.py
@@ -7,8 +7,12 @@ Description: Write a program that uses nested loops to print the multiplication 
 
 # Use a for loop to iterate through the numbers for each table
 
+for i in range(1,6):
+    print(f"Multiplication Table for {i}:")
 
     # Use a nested loop to print the table for each number
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i * j}")
     
 
     
